551df62890b089ef92f132c70aedc8c064c92f5a
hope-uwa <ingenioushope@gmail.com>
2025-05-01 23:59:29 -0700

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Coming Soon...</h1>
+      <p className="read-the-docs">
+        Giving made easy.
+      </p>
+    </>
+  )
+}
+
+export default App
